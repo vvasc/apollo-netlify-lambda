43f006bcf775c7cefff426ef8e557210b3310d32
vvasc <vinedias@gmail.com>
2019-11-11 00:11:06 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
 
 class LambdaDemo extends Component {
   constructor(props) {
@@ -8,25 +11,24 @@ class LambdaDemo extends Component {
     this.state = { loading: false, msg: null }
   }
 
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
   render() {
-    const { loading, msg } = this.state
+    const client = new ApolloClient({
+      uri: "/.netlify/functions/graphql"
+    });
 
     return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
+      <ApolloProvider client={client}>
+        <Query
+          query={gql`
+            {
+              hello
+            }
+          `}
+        >
+          {({ data }) =>
+            <div>A greeting from the server: {data.hello}</div>}
+        </Query>
+      </ApolloProvider>
     )
   }
 }
